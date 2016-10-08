c962eb4875ff58871be16e52d1b8cf376b42f245
TheDropOne <TheDropOne@gmail.com>
2016-10-08 03:38:13 +0300

    Done few Tasks: 1 --- 100% 2 --- 100% 3 --- 50% 4 --- 33% 5 --- 0% 6 ---100% 7 --- 100% 8 --- 0% 9 --- 0%

--- src/data/TestLists.java
+++ src/data/TestLists.java
@@ -1,5 +1,6 @@
 package data;
 
+import tests.ComplexTest;
 import tests.SimpleTest;
 
 import java.io.File;
@@ -13,27 +14,28 @@ import static iostream.MyFileReader.*;
  */
 public class TestLists {
 
-    public static List<SimpleTest> tests_first_task;
+    public static List<ComplexTest> tests_first_task;
     public static List<SimpleTest> tests_second_task;
     public static List<SimpleTest> tests_third_task;
     public static List<SimpleTest> tests_fourth_task;
     public static List<SimpleTest> tests_fifth_task;
     public static List<SimpleTest> tests_sixth_task;
     public static List<SimpleTest> tests_seventh_task;
-    public static List<SimpleTest> tests_eighth_task;
-    public static List<SimpleTest> tests_ninth_task;
+    public static List<ComplexTest> tests_eighth_task;
+    public static List<ComplexTest> tests_ninth_task;
 
     public static void uploadTests(){
         try {
-            tests_first_task = readSimpleTestFromFile(new File("test1"));
-            tests_second_task = readSimpleTestFromFile(new File("test2"));
-            tests_third_task = readSimpleTestFromFile(new File("test3"));
-            tests_fourth_task = readSimpleTestFromFile(new File("test4"));
-            tests_fifth_task = readSimpleTestFromFile(new File("test5"));
-            tests_sixth_task = readSimpleTestFromFile(new File("test6"));
-            tests_seventh_task = readSimpleTestFromFile(new File("test7"));
-            tests_eighth_task = readSimpleTestFromFile(new File("test8"));
-            tests_ninth_task = readSimpleTestFromFile(new File("test9"));
+            tests_second_task = readSimpleTestFromFile(new File("test2.txt"));
+            tests_third_task = readSimpleTestFromFile(new File("test3.txt"));
+            tests_fourth_task = readSimpleTestFromFile(new File("test4.txt"));
+            tests_fifth_task = readSimpleTestFromFile(new File("test5.txt"));
+            tests_sixth_task = readSimpleTestFromFile(new File("test6.txt"));
+            tests_seventh_task = readSimpleTestFromFile(new File("test7.txt"));
+
+            tests_first_task = readComplexTestFromFile(new File("test1.txt"));
+            tests_eighth_task = readComplexTestFromFile(new File("test8.txt"));
+            tests_ninth_task = readComplexTestFromFile(new File("test9.txt"));
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
--- src/entity/Tester.java
+++ src/entity/Tester.java
@@ -23,28 +23,67 @@ public class Tester {
             isMatches = matcher.matches();
             reportPrinter.addLine(test.getExpression() + "\t\t" + isMatches);
             if (isMatches != test.isCorrect()) {
+                reportPrinter.addLine(test.getExpression() + "\t\t" + "test faled!");
                 throw new TestNotPassedException("\t SimpleTest " + test + " not passed!");
             }
         }
+        reportPrinter.addLine("All tests passed!");
         reportPrinter.printReport();
     }
 
-    /*
-    public static void makeComplexTest(List<ComplexTest> testList, String regex) throws TestNotPassedException {
+
+    public static void makeComplexTest(List<ComplexTest> testList, String regex, int testNumber) throws TestNotPassedException {
+        if (testNumber != 1 && testNumber != 8 && testNumber != 9) return;
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher;
         ReportPrinter reportPrinter = new ReportPrinter();
         boolean isMatches;
         for (ComplexTest test : testList) {
-            matcher = pattern.matcher(test.getExpression());
-            isMatches = matcher.matches();
-            reportPrinter.addLine(test.getExpression() + "\t\t" + isMatches);
-            if (isMatches != test.isCorrect()) {
-                throw new TestNotPassedException("\t SimpleTest " + test + " not passed!");
+            switch (testNumber) {
+                case 1:
+                    String tempString = "";
+                    matcher = pattern.matcher(test.getExpression());
+                    if (matcher.matches()) {
+                        tempString = matcher.group();
+                        tempString = tempString.substring(1, tempString.length() - 1);
+                    }
+                    tempString = getStringFirstTask(tempString);
+                    isMatches = tempString.equals(test.getCorrectExpression());
+                    if (isMatches) {
+                        reportPrinter.addLine(test.getExpression() + "\t\t" + "Test passed successfully");
+                    } else {
+                        reportPrinter.addLine(test.getExpression() + "\t\t" + "Test faled!");
+                        throw new TestNotPassedException("\t SimpleTest " + test + " not passed!");
+                    }
+                    break;
+                case 8:
+                    if (true) {
+                        throw new TestNotPassedException("\t SimpleTest " + test + " not passed!");
+                    }
+                    break;
+                case 9:
+                    if (true) {
+                        throw new TestNotPassedException("\t SimpleTest " + test + " not passed!");
+                    }
+                    break;
             }
+
         }
+        reportPrinter.addLine("All tests passed!");
         reportPrinter.printReport();
     }
-    */
+
+
+    private static String getStringFirstTask(String result) {
+        return (!result.isEmpty()) ? "<em>" + result + "<\\em>" : "wrong";
+    }
+
+    private static String getStringEighthTask(String result) {
+        return "<em>" + result + "<\\em>";
+    }
+
+    private static String getStringNinthTask(String result) {
+        return result;
+    }
 
 }
