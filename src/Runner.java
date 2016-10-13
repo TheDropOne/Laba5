861b5ffde00cc5f41437b6258b2ec853aa587e75
TheDropOne <TheDropOne@gmail.com>
2016-10-14 00:49:15 +0300

    5th task complete at all!

--- src/Runner.java
+++ src/Runner.java
@@ -15,7 +15,8 @@ public class Runner {
     private static final String FOURTH_REGEX = "((([01]?\\d{2}|2[0-4]\\d|25[0-5])\\.?){3}([01]?\\d{2}|2[0-4]\\d|25[0-5]))" +
             "|((([01]?\\d{2}|2[0-4]\\d|25[0-6])\\.?){3}([01]?\\d{2}|2[0-4]\\d|25[0-6]))" +
             "|((0?(\\d{1,3}|x[a-fA-F0-9]{2})\\.){3}0?(\\d{1,3}|x[a-fA-F0-9]{1,2}))"; // All Tests Passed!
-    private static final String FIFTH_REGEX = "(https?:\\\\)?(w{3}\\.)([a-zA-Z]([^\\s_]+)?[a-zA-Z])?";
+    private static final String FIFTH_REGEX = "^((https?://)?(w{3}\\.)?)?([a-zA_Z][a-zA-Z\\+\\-]{0,61}[a-zA_Z]\\.)+([a-z]{2,4})" +
+            "(:\\d{2,4}|((/[^\\s\\.]+)+)\\.[a-z]{2,4}((\\?((\\w+=\\S+\\&)+)?(\\w+=\\S+))|#\\w+)?)?";
     private static final String SIXTH_REGEX = "(0[1-9]|[12]\\d|3[01])/(0[1-9]|1[0-2])/(1[6-9]\\d{2}|[2-9]\\d{3})"; // All Tests Passed!
     private static final String SEVENTH_REGEX = "#[a-fA-F0-9]{6}"; // All Tests Passed!
     private static final String EIGHTH_REGEX = "([^\\s";
@@ -31,7 +32,9 @@ public class Runner {
         //Pattern p4 = Pattern.compile("((([01]?\\d{2}|2[0-4]\\d|25[0-5])\\.?){3}([01]?\\d{2}|2[0-4]\\d|25[0-5]))" +
         //        "|((([01]?\\d{2}|2[0-4]\\d|25[0-6])\\.?){3}([01]?\\d{2}|2[0-4]\\d|25[0-6]))" +
         //        "|((0?(\\d{1,3}|x[a-fA-F0-9]{2})\\.){3}0?(\\d{1,3}|x[a-fA-F0-9]{1,2}))");
-        Pattern p5 = Pattern.compile("^((https?://)?(w{3}\\.)?)?([a-zA_Z][a-zA-Z\\+\\-]{1,61}[a-zA_Z]\\.)+([a-z]{2,4})");
+
+        //Pattern p5 = Pattern.compile("^((https?://)?(w{3}\\.)?)?([a-zA_Z][a-zA-Z\\+\\-]{0,61}[a-zA_Z]\\.)+([a-z]{2,4})" +
+        //        "(:\\d{2,4}|((/[^\\s\\.]+)+)\\.[a-z]{2,4}((\\?((\\w+=\\w+\\&)+)?(\\w+=\\w+))|#\\w+)?)?");
         //Pattern p6 = Pattern.compile("(0[1-9]|[12]\\d|3[01])/(0[1-9]|1[0-2])/(1[6-9]\\d{2}|[2-9]\\d{3})");
         //Pattern p7 = Pattern.compile("#[a-fA-F0-9]{6}");
     }
@@ -44,7 +47,7 @@ public class Runner {
             Tester.makeSimpleTest(TestLists.tests_second_task, SECOND_REGEX);
             //Tester.makeSimpleTest(TestLists.tests_third_task, THIRD_REGEX);
             Tester.makeSimpleTest(TestLists.tests_fourth_task, FOURTH_REGEX);
-            //Tester.makeSimpleTest(TestLists.tests_fifth_task, FIFTH_REGEX);
+            Tester.makeSimpleTest(TestLists.tests_fifth_task, FIFTH_REGEX);
             Tester.makeSimpleTest(TestLists.tests_sixth_task, SIXTH_REGEX);
             Tester.makeSimpleTest(TestLists.tests_seventh_task, SEVENTH_REGEX);
             System.exit(0);
