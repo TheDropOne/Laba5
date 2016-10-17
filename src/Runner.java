91d71efccc14b58c1c733d36e84f1c31e04ddcd9
TheDropOne <TheDropOne@gmail.com>
2016-10-17 19:24:11 +0300

    Some bugs fixed

--- src/Runner.java
+++ src/Runner.java
@@ -15,7 +15,7 @@ public class Runner {
             "|\\{[a-fA-F0-9]{8}-([a-fA-F0-9]{4}-){3}[a-fA-F0-9]{12}\\}"; // All Tests Passed!
     private static final String FOURTH_REGEX = "((([01]?\\d{2}|2[0-4]\\d|25[0-5])\\.?){3}([01]?\\d{2}|2[0-4]\\d|25[0-5]))" +
             "|((([01]?\\d{2}|2[0-4]\\d|25[0-5])\\.?){3}([01]?\\d{2}|2[0-4]\\d|25[0-5]))" +
-            "|((0?(\\d{1,3}|x[a-fA-F0-9]{2})\\.){3}0?(\\d{1,3}|x[a-fA-F0-9]{1,2}))"; // All Tests Passed!
+            "|((((0\\d{1,3})|([01]?\\d{2}|2[0-4]\\d|25[0-5])|x[a-fA-F0-5]{2})\\.){3}0?(\\d{1,3}|x[a-fA-F0-5]{1,2}))"; // All Tests Passed!
     private static final String FIFTH_REGEX = "^((https?://)?(w{3}\\.)?)?([a-zA_Z][a-zA-Z\\+\\-]{0,61}[a-zA_Z]\\.)+([a-z]{2,4})" +
             "(:\\d{2,4}|((/[^\\s\\.]+)+)\\.[a-z]{2,4}((\\?((\\w+=\\S+\\&)+)?(\\w+=\\S+))|#\\w+)?)?"; // All Tests Passed!
     private static final String SIXTH_REGEX = "((0[1-9]|[12]\\d|3[01])/(0[13578]|1[02])|(0[1-9]|[12]\\d|30)/(0[469]|11)|(0[1-9]|[12][0-8])/02)/(1[6-9]\\d{2}|[2-9]\\d{3})" +
@@ -26,7 +26,7 @@ public class Runner {
             "[^\\s\\*\"/\\?\\<>\\:\\\\]+" +     //nickname (Required)
             "\\s+PRIVMSG\\s+" +         //PRIVMSG command (Required)
             "\\#\\p{Lower}{2,}\\s+" +   //channel (Required)
-            "\\:.{1,450}";               // message (Required)
+            "\\:.{1,450}";               // message (Required) All Tests Passed
 
 
     public static void main(String[] args) {
@@ -37,15 +37,15 @@ public class Runner {
 
             Tester.makeSimpleTest(TestLists.tests_second_task, SECOND_REGEX);       // 24
             Tester.makeSimpleTest(TestLists.tests_third_task, THIRD_REGEX);         // 105
-            Tester.makeSimpleTest(TestLists.tests_fourth_task, FOURTH_REGEX);       // 203
+            Tester.makeSimpleTest(TestLists.tests_fourth_task, FOURTH_REGEX);       // 234
             Tester.makeSimpleTest(TestLists.tests_fifth_task, FIFTH_REGEX);         // 150
-            Tester.makeSimpleTest(TestLists.tests_sixth_task, SIXTH_REGEX);         // 62
+            Tester.makeSimpleTest(TestLists.tests_sixth_task, SIXTH_REGEX);         // 209
             Tester.makeSimpleTest(TestLists.tests_seventh_task, SEVENTH_REGEX);     // 15
 
             Tester.makeComplexTest(TestLists.tests_eighth_task, EIGHTH_REGEX, 8);   // 61
             Tester.makeSimpleTest(TestLists.tests_ninth_task, NINTH_REGEX);         // 119
 
-                                    //0.2703757542
+                                    //0.2583788068 additional points
         } catch (TestNotPassedException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
